20230df3cebcd7b439308d2edad704f859aea3e8
issara mantapa <issamanx2307@gmail.com>
2026-03-22 20:08:58 +0700

    chore: cleanup temp files

---
+++ _commit.bat
@@ -0,0 +1,5 @@
+@echo off
+cd /d D:\stockradar
+git add -A
+git commit -m "chore: cleanup temp files"
+echo Committed.
--- gitcommit.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d D:\stockradar
-git add -A
-git commit -m "feat: StockRadar v5 production ready"
-echo Done.
