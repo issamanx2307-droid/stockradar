bce8290758931980c2393c9dccc8bd57fb7727ca
issara mantapa <issamanx2307@gmail.com>
2026-03-26 11:45:40 +0700

    fix: import permission_classes decorator in views.py

--- radar/views.py
+++ radar/views.py
@@ -12,7 +12,7 @@ from django.db import models
 from django.db.models import Subquery, OuterRef
 from django.utils import timezone
 from rest_framework import generics, filters
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 
